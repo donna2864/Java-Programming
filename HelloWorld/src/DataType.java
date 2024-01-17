3a562a2d0b7305f1a5cf3eca5450284527af35ae
Donna R <112607918+donna2864@users.noreply.github.com>
2024-01-17 14:48:57 +0530

    data types in java

---
+++ HelloWorld/src/DataType.java
@@ -0,0 +1,16 @@
+public class DataType {
+    public static void main(String[] args){
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        System.out.println("Max value of int: "+max);
+        System.out.println("Min value of int: "+min);
+
+        byte maxByteValue = Byte.MAX_VALUE;
+        byte minByteValue = Byte.MIN_VALUE;
+
+        System.out.println("Max byte value: " + maxByteValue);
+        System.out.println("Min byte value: " + minByteValue);
+
+    }
+}
