b7369b60d2f906f820d512206fda13d8ab55b1c7
Donna R <112607918+donna2864@users.noreply.github.com>
2024-01-17 15:03:18 +0530

    data types in java

--- HelloWorld/src/DataType.java
+++ HelloWorld/src/DataType.java
@@ -1,5 +1,6 @@
 public class DataType {
     public static void main(String[] args){
+        // store whole numbers
         int max = Integer.MAX_VALUE;
         int min = Integer.MIN_VALUE;
 
@@ -11,6 +12,26 @@ public class DataType {
 
         System.out.println("Max byte value: " + maxByteValue);
         System.out.println("Min byte value: " + minByteValue);
+
+        long myNumber = 2147483647L;
+
+        //fractional Number
+        short maxi = Short.MAX_VALUE;
+        short mini = Short.MIN_VALUE;
+
+        System.out.println(maxi);
+        System.out.println(mini);
+
+        float num = 3.14f; // for floating point you need to end the number with a 'f', else it is a double
+        double number1 = 2.0;
+
+        // character types
+        char letter = 'A'; //only one character can have only one letter stored in it.
+        System.out.println(letter);
+
+        // boolean types
+        boolean isActive = false;
+        System.out.println(isActive);
 
     }
 }
