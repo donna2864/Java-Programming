2c95462c1d81e0777b5e1a35560e0653aada8454
Donna R <112607918+donna2864@users.noreply.github.com>
2024-01-17 15:04:23 +0530

    data types in java

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,5 +1,8 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello World!");
+public class Main {
+    public static void main(String[] args) {
+        String name = "Donna R";
+        int age = 19;
+        System.out.println("Hello World!" + name);
+        System.out.println("Your age is " + age);
     }
 }
